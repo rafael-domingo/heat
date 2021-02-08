75916ed4f4eb405fd7b79791111b0c64b39ce7f1
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-02-07 21:50:26 -0600

    Configure search to initiate after pressing Enter

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -11,9 +11,13 @@ export default class Search extends React.Component {
         this.handleTermChange = this.handleTermChange.bind(this);
     }
 
-    search() {
-        this.props.search(this.state.term);
-        console.log(this.state.term)
+    search(e) {
+        // check if enter is key entered
+        if (e.keyCode === 13) {
+            this.props.search(this.state.term);
+            console.log(this.state.term)
+        }
+        
     }
 
     handleTermChange(e) {
@@ -26,7 +30,7 @@ export default class Search extends React.Component {
         return (
             <div>
                 <div class="form">
-                    <input name="city" type="text" onChange={this.handleTermChange} required/>
+                    <input name="city" type="text" onChange={this.handleTermChange} onKeyDown={this.search} required/>
                     <label for="city" class="label-name">
                         <span className="content-name">Search for a city</span>
                     </label>
