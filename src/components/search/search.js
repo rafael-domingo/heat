9c45ca864d1f8d04f6180fb8a0d741ce4419ebcf
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-02-10 22:30:00 -0600

    Refactor code so async fetch only occurs once instead of every event change

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,6 +7,7 @@ export default class Search extends React.Component {
         this.state = {
             term: '',
             error: '',
+            cityList: [],
             filteredOptions: [],
             showOptions: false
         }
@@ -15,9 +16,11 @@ export default class Search extends React.Component {
     }
 
     search(e) {
-        // check if enter is key entered and string is not empty
+        // TODO: refactor this to search when selecting city from list
+
         if (e.keyCode === 13 && this.state.term.length > 0) {
             this.props.search(this.state.term);
+            this.props.render(true);
             console.log(this.props.error);
             this.setState(prevState => ({
                 ...prevState,
@@ -29,6 +32,7 @@ export default class Search extends React.Component {
                 valid: false,
                 error: 'enter a city name'
             }))
+            this.props.render(false);
         } else {
             this.setState(prevState => ({
                 ...prevState,
@@ -48,35 +52,57 @@ export default class Search extends React.Component {
         }).then(response => {
             return response.json();
         }).then(jsonResponse => {
+            this.setState({cityList: jsonResponse});
+            console.log(jsonResponse);
             return jsonResponse;
         })
     }
 
     handleTermChange(e) {
         // Check input versus list of cities to populate autocomplete list
-        this.autoComplete().then(list => {
-            var userInput = e.target.value;
-            var cityList = list.filter(option => option.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1);
-            console.log(cityList);
-            var cityListArray = cityList.map(city => <p>{city.name}</p>);
+        if (e.target.value.indexOf(' ') !== 0) {
+            if (e.target.value.length > 1 || e.target.value.indexOf(' ') >= 1) {
+                var userInput = e.target.value;
+                var list = this.state.cityList.filter(option => option.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1);
+                var cityListArray = list.map(city => <p>{city.name}</p>);
+                this.setState(prevState => ({
+                    ...prevState,
+                    term: e.target.value,
+                    error: '',
+                    filteredOptions: cityListArray
+                }))
+            } else {
+                // TODO: clean up these if/else statements
+                this.setState(prevState => ({
+                    ...prevState,
+                    term: e.target.value,
+                    error: '',
+                    filteredOptions: []
+                }))
+                this.props.render(false);
+            }            
+        } else {
             this.setState(prevState => ({
                 ...prevState,
                 term: e.target.value,
                 error: '',
-                filteredOptions: cityListArray
+                filteredOptions: []
             }))
-        })
-       
-        this.props.render(false);
-        
+            this.props.render(false);
+        } 
+        }
+    componentDidMount() {
+        window.addEventListener('load', this.autoComplete());
     }
+
+        
     render() {
         return (
             <div className="container-form">
            <div className="form">
                 <input name="city" type="text" onChange={this.handleTermChange} onKeyDown={this.search} required/>
                 <label htmlFor="city" className="label-name">
-                    <span className="content-name" style={{opacity: !this.props.error ? 1 : 0}}>Search for a city</span>
+                    <span className="content-name">Search for a city</span>
                 </label>
             </div>
                 {/* <p style={{opacity: !this.props.error ? 0 : 1, textAlign: 'left'}}>{this.state.error}</p> */}
